7553dd3ca2cbb5e60f0632a987b7c12db092efb6
Josh Keegan <josh.keegan@gmx.com>
2017-10-02 17:14:19 +0100

    Implement tests against the GroundTruth datasets from Python & Java implementations

--- VaderSharp/VaderSharp/SentimentAnalysisResults.cs
+++ VaderSharp/VaderSharp/SentimentAnalysisResults.cs
@@ -1,10 +1,14 @@
-﻿namespace VaderSharp
+﻿using System;
+
+namespace VaderSharp
 {
     /// <summary>
     /// A model to represent the result of analysis.
     /// </summary>
     public class SentimentAnalysisResults
     {
+        private const double TOLERANCE = 0.05;
+
         /// <summary>
         /// The proportion of words in the sentence with negative valence.
         /// </summary>
@@ -24,5 +28,28 @@
         /// Normalized sentiment score between -1 and 1.
         /// </summary>
         public double Compound { get; set; }
+
+        public override bool Equals(object o)
+        {
+            SentimentAnalysisResults r = o as SentimentAnalysisResults;
+
+            if (r == null)
+            {
+                return false;
+            }
+
+            return Math.Abs(r.Negative - Negative) < TOLERANCE && Math.Abs(r.Neutral - Neutral) < TOLERANCE &&
+                   Math.Abs(r.Positive - Positive) < TOLERANCE && Math.Abs(r.Compound - Compound) < TOLERANCE;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -1024570712;
+            hashCode = hashCode * -1521134295 + Negative.GetHashCode();
+            hashCode = hashCode * -1521134295 + Neutral.GetHashCode();
+            hashCode = hashCode * -1521134295 + Positive.GetHashCode();
+            hashCode = hashCode * -1521134295 + Compound.GetHashCode();
+            return hashCode;
+        }
     }
 }
---
+++ VaderSharp/VaderSharpTestCore/PortTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VaderSharp;
+
+namespace VaderSharpTestCore
+{
+    /// <summary>
+    /// Tests the port to C# by running the Ground Truth data sets from the python implementation,
+    /// which contains expected results, against the results from the C# implementation
+    /// </summary>
+    [TestClass]
+    public class PortTests
+    {
+        // Constants
+        private const char DELIMITER = '\t';
+
+        // Variables
+        private static string[] filePaths;
+
+        // Set Up & tear Down
+        [TestInitialize]
+        public void Initialize()
+        {
+            // These TSVs have been taken from the Java implementation's tests: https://github.com/apanimesh061/VaderSentimentJava/tree/master/src/test/resources
+            // If that isn't maintained in future, they could also be generated from the original Python implementation: https://github.com/cjhutto/vaderSentiment
+            filePaths = Directory.GetFiles("../../../Resources/GroundTruth");
+        }
+
+        // Tests
+        [TestMethod]
+        public void GroundTruthTest()
+        {
+            SentimentIntensityAnalyzer sa = new SentimentIntensityAnalyzer();
+
+            foreach (string path in filePaths)
+            {
+                using (FileStream stream = File.Open(path, FileMode.Open))
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        // Split the line around the delimiter
+                        string[] parts = line.Split(DELIMITER);
+
+                        // Get the input & scores
+                        double expectedNeg = double.Parse(parts[1]);
+                        double expectedNeu = double.Parse(parts[2]);
+                        double expectedPos = double.Parse(parts[3]);
+                        double expectedCom = double.Parse(parts[4]);
+                        string input = parts[5];
+
+                        SentimentAnalysisResults expected = new SentimentAnalysisResults()
+                        {
+                            Negative = expectedNeg,
+                            Neutral = expectedNeu,
+                            Positive = expectedPos,
+                            Compound = expectedCom
+                        };
+
+                        // Run SA on input
+                        SentimentAnalysisResults actual = sa.PolarityScores(input);
+
+                        Assert.AreEqual(expected, actual);
+                    }
+                }
+            }
+        }
+    }
+}
