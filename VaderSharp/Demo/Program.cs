8903d3d9268f9507b286401ac1ca5858f620e7cc
Josh Keegan <josh.keegan@gmx.com>
2017-10-02 17:25:20 +0100

    Add demo project

---
+++ VaderSharp/Demo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VaderSharp;
+
+namespace Demo
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            SentimentIntensityAnalyzer sa = new SentimentIntensityAnalyzer();
+            SentimentAnalysisResults results = sa.PolarityScores("ok if you have a 2nd dvd player.");
+
+            Console.WriteLine("Positive score: " + results.Positive);
+            Console.WriteLine("Negative score: " + results.Negative);
+            Console.WriteLine("Neutral score: " + results.Neutral);
+            Console.WriteLine("Compound score: " + results.Compound);
+        }
+    }
+}
